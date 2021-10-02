db72c943a9d09436b169cc81ced3db2227a54749
IK Cranston <ikcranston@outlook.com>
2021-10-02 15:58:41 -0400

    refactor: Formatting changes to R script
    Minor formatting changes
    Change hotf_mass.csv to export to relative path

--- R Scripts/hamburgers of the forest pilot analysis.R
+++ R Scripts/hamburgers of the forest pilot analysis.R
@@ -19,10 +19,12 @@ hotf_dat <- read.csv("./Data/Originals/pilot branch data.csv")
 # pilot glmms ####
 # make a new column that is "native" vs "non-native"
 # make a guiding table
-native_dat <- tibble(id = c("Honeysuckle", "Barberry", "Burning Bush", "Autumn Olive", "Musclewood", "Sweet Birch", "Beech", "Black Cherry", "Witch-hazel", "Striped Maple"), 
-              exo = c("Non-native", "Non-native", "Non-native", "Non-native", "Native", "Native", "Native", "Native", "Native", "Native"))
+native_dat <- tibble(
+  id = c("Honeysuckle", "Barberry", "Burning Bush", "Autumn Olive", "Musclewood", "Sweet Birch", "Beech", "Black Cherry", "Witch-hazel", "Striped Maple"),
+  exo = c("Non-native", "Non-native", "Non-native", "Non-native", "Native", "Native", "Native", "Native", "Native", "Native")
+)
 
-# merge guiding table and raw data 
+# merge guiding table and raw data
 hotf_dat <- hotf_dat %>%
   left_join(y = native_dat, by = c(tree = "id"))
 
@@ -39,79 +41,71 @@ hotf_summary
 hotf_dat$wet_mass_g <- as.numeric(hotf_dat$wet_mass_g)
 hotf_dat$time_block <- as.factor(hotf_dat$time_block)
 
-write.csv(hotf_summary, "hotf_mass.csv")
+write.csv(hotf_summary, "./Data/Output/hotf_mass.csv")
 
 # multiple high values
 arrange(hotf_summary, -sum_wet_mass)
-
-# 
 
 # drop 7.74 and 3.08
 hotf_summary <- subset(hotf_summary, sum_wet_mass < 2)
 
 
 # do the wet mass glm with sum of masses
-hotf_glm <- glm(sum_wet_mass ~ tree * treatment, data=hotf_summary)
+hotf_glm <- glm(sum_wet_mass ~ tree * treatment, data = hotf_summary)
 
 summary(hotf_glm)
 Anova(hotf_glm)
 
-plot(emmeans(hotf_glm, ~ tree, type="response"))
-plot(emmeans(hotf_glm, ~ treatment, type="response"))
-plot(emmeans(hotf_glm, ~ treatment*tree, type="response"))
-emmeans(hotf_glm, ~ treatment)
+plot(emmeans(hotf_glm, ~tree, type = "response"))
+plot(emmeans(hotf_glm, ~treatment, type = "response"))
+plot(emmeans(hotf_glm, ~ treatment * tree, type = "response"))
+emmeans(hotf_glm, ~treatment)
 
 # non-pooled biomass
 arrange(hotf_dat, -wet_mass_g)
 hotf_chop <- subset(hotf_dat, wet_mass_g < 2)
 
 
-hotf_glm_1 <- lmer(log(wet_mass_g) ~ tree*treatment + (1|branch_code), data=hotf_chop)
+hotf_glm_1 <- lmer(log(wet_mass_g) ~ tree * treatment + (1 | branch_code), data = hotf_chop)
 Anova(hotf_glm_1)
-plot(emmeans(hotf_glm_1, ~ treatment*tree), type="response")
+plot(emmeans(hotf_glm_1, ~ treatment * tree), type = "response")
 
-cld(emmeans(hotf_glm_1, ~treatment|tree), type="response", adjust="none")
-
-
-
-
-
-
+cld(emmeans(hotf_glm_1, ~ treatment | tree), type = "response", adjust = "none")
 
 
 # native vs. non-native
-native_glm_1 <- lmer(log(wet_mass_g) ~ exo*treatment + (1|branch_code), data=hotf_dat)
+native_glm_1 <- lmer(log(wet_mass_g) ~ exo * treatment + (1 | branch_code), data = hotf_dat)
 Anova(native_glm_1)
 
-plot(emmeans(native_glm_1, ~ treatment*exo), type="response")
+plot(emmeans(native_glm_1, ~ treatment * exo), type = "response")
 
 # bug count
 hotf_dat$bug_count <- as.numeric(hotf_dat$bug_count)
 
 
-hotf_glm_2 <- glmer(bug_count ~ tree*treatment + time_block + (1|branch_code), family="poisson", data=hotf_dat)
+hotf_glm_2 <- glmer(bug_count ~ tree * treatment + time_block + (1 | branch_code), family = "poisson", data = hotf_dat)
 Anova(hotf_glm_2)
 summary(hotf_glm_2)
 
-plot(emmeans(hotf_glm_2, ~ treatment*tree, type="response"))
+plot(emmeans(hotf_glm_2, ~ treatment * tree, type = "response"))
 
-cld(emmeans(hotf_glm_2, ~ treatment|tree))
+cld(emmeans(hotf_glm_2, ~ treatment | tree))
 
-plot(emmeans(hotf_glm_2, ~ treatment, type="response"))
-plot(emmeans(hotf_glm_2, ~ tree, type="response"))
-plot(emmeans(hotf_glm_2, ~ time_block, type="response"))
+plot(emmeans(hotf_glm_2, ~treatment, type = "response"))
+plot(emmeans(hotf_glm_2, ~tree, type = "response"))
+plot(emmeans(hotf_glm_2, ~time_block, type = "response"))
 
 
-hotf_glm_3 <- glmer.nb(morphospecies ~ tree + treatment + (1|branch_code), data=hotf_dat)
+hotf_glm_3 <- glmer.nb(morphospecies ~ tree + treatment + (1 | branch_code), data = hotf_dat)
 Anova(hotf_glm_3)
 
 # full model (interaction terms and time block had AIC of 3959.811)
 # reduced model (just two treatment terms, has 3943.67)
 AIC(hotf_glm_3)
 
-plot(emmeans(hotf_glm_3, ~ treatment*tree, type="response"))
+plot(emmeans(hotf_glm_3, ~ treatment * tree, type = "response"))
 
-plot(emmeans(hotf_glm_3, ~ tree, type="response"))
+plot(emmeans(hotf_glm_3, ~tree, type = "response"))
 
 # pilot leaf analysis #####
 
@@ -123,18 +117,18 @@ hist(leaf_dat$leaf.count)
 leaf_dat$leaf.count <- log(leaf_dat$leaf.count)
 
 # poisson is terrifically bad, data is log-normal
-leaf_glm <- glm(log(leaf.count) ~ tree/(treatment + log(bug_count) + log(morphospecies.) + log(wet_mass_g)), data=leaf_dat)
+leaf_glm <- glm(log(leaf.count) ~ tree / (treatment + log(bug_count) + log(morphospecies.) + log(wet_mass_g)), data = leaf_dat)
 Anova(leaf_glm)
 summary(leaf_glm)
 
-plot(emmeans(leaf_glm, ~ treatment|tree, nesting = NULL, type="response"))
-plot(emmeans(leaf_glm, ~ treatment|tree|bug_count, nesting = NULL))
-plot(emmeans(leaf_glm, ~ treatment|morphospecies., nesting = NULL))
-plot(emmeans(leaf_glm, ~ treatment|wet_mass_g, nesting = NULL))
-plot(emmeans(leaf_glm, ~tree, type="response" ))
+plot(emmeans(leaf_glm, ~ treatment | tree, nesting = NULL, type = "response"))
+plot(emmeans(leaf_glm, ~ treatment | tree | bug_count, nesting = NULL))
+plot(emmeans(leaf_glm, ~ treatment | morphospecies., nesting = NULL))
+plot(emmeans(leaf_glm, ~ treatment | wet_mass_g, nesting = NULL))
+plot(emmeans(leaf_glm, ~tree, type = "response"))
 
 # emmean reg plots ####
-emmip(leaf_glm, ~ morphospecies.|tree, cov.reduce = range, type="response")
+emmip(leaf_glm, ~ morphospecies. | tree, cov.reduce = range, type = "response")
 
 hist(log(leaf_dat$bug_count))
 hist(log(leaf_dat$morphospecies.))
@@ -144,11 +138,11 @@ hist(log(leaf_dat$wet_mass_g))
 
 # example model selection to see if we can drop leaf count
 
-yup.glm <- glm(wet_mass_g ~ tree * treatment + leaf.count, data=leaf_dat)
+yup.glm <- glm(wet_mass_g ~ tree * treatment + leaf.count, data = leaf_dat)
 Anova(yup.glm)
 AIC(yup.glm)
 
-yup.glm.2 <- glm(wet_mass_g ~ treatment, data=leaf_dat)
+yup.glm.2 <- glm(wet_mass_g ~ treatment, data = leaf_dat)
 AIC(yup.glm.2)
 
 anova(yup.glm, yup.glm.2)
