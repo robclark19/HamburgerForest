abedb86e622cde3aa017530bddfd1d1c38009c8e
robclark19 <robert.e.clark@wsu.edu>
2022-04-02 09:50:59 -0400

    Start outline.

---
+++ R Scripts/trophic project manuscript analyses.R
@@ -0,0 +1,28 @@
+# Trophic project 2021 Primary Figures
+# Libraries ####
+
+
+
+
+
+# Data ######
+
+
+# Figure 1: effects of host plants and birds on arthropod biomass
+
+
+
+
+# Figure 2: bag effects on different feeding guilds
+# What insects birds are eating on invasives
+
+
+
+
+# Figure 3: C:N content among native and non-native plants for spiders and insect herbivores
+
+
+
+
+
+# Figures s1-s4: Major insect groups variation in counts among host plants
