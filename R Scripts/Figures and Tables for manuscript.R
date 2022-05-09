3d1766b1d5efd99cc234c9fa5bd42ce756e154ea
Robert Clark <you@example.com>
2022-05-09 15:14:24 -0400

    Start refactoring code for final analyses.

--- R Scripts/Figures and Tables for manuscript.R
+++ R Scripts/Figures and Tables for manuscript.R
@@ -10,3 +10,34 @@ library("ggplot2")
 library("emmeans")
 library("ggpubr")
 library("plotrix") # for std.error function
+
+# Fig 1a: Bagged branch biomass ####
+
+
+
+
+
+# Fig 1b: Bag effect ######
+# Import model estimates and table
+mod2_lsm <- read.csv("./Data/Models/model2.csv")
+
+Fig_1b <- ggplot(data=mod2_lsm, aes(x = treatment, y = response)) +
+  theme_bw(base_size=12) +
+  geom_point(size=2) +
+  geom_line(aes(group = tree)) +
+  geom_errorbar(aes(ymin=response-(SE), ymax=response+(SE), width=0)) +
+  ylab("Average arthropod biomass (g)") +
+  xlab("Bird Exclusion") +
+  scale_x_discrete(labels=c("bag" = "- Birds", "control" = "+ Birds")) +
+  geom_text(aes(x = treatment, y = (response+SE), label = .group, hjust=-.5)) +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+  facet_wrap( ~ tree, nrow=2) 
+Fig_1b
+
+# export Fig 1b #
+
+
+
+
+# Fig 2abcd #####
+# Insect abundances #####
--- R Scripts/Models for manuscript.R
+++ R Scripts/Models for manuscript.R
@@ -9,3 +9,41 @@ library("multcomp")
 library("emmeans")
 library("plotrix") # for std.error function
 
+# Data ####
+ht_dat <- read.csv(file="./Data/Output/manuscript_dat.csv")
+
+
+# Manuscript Body Analyses #####
+# These are models that go into the main figures or are connected to the main text of the manuscript
+
+# Model 1: Bagged branch biomass #####
+
+
+
+
+
+
+
+
+
+
+# Model 2: Bag effects #####
+# Figure 1: Bird effect across 10 host plants
+model_2 <- lmer(log(wet_mass_g) ~ tree * treatment +  (1 | branch_code), data = ht_dat)
+
+# Table for Figure 1
+Anova(model_2)
+
+# Parameter estimates and posthoc tests
+# this should be "pairs" and report p-values
+tree.lsm <- emmeans(model_2, ~ treatment | tree, type = "response") %>% 
+  cld(adjust="scheffe", Letters=c("abcd")) %>%
+  as.data.frame()
+
+# Write the lsm object to a .csv for ggplot to use
+write.csv(tree.lsm, "./Data/Models/model2.csv")
+
+
+# Supplemental Analyses #####
+# These are related analyses that are primarily found in the appendix and NOT the body of the manuscript
+
