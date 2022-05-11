5b43168354e3d4a783dc29fdc408db6cebf00715
Robert Clark <you@example.com>
2022-05-11 15:47:54 -0400

    Refactor biomass GLM script and add posthoc tests to figure.

--- R Scripts/Figures and Tables for manuscript.R
+++ R Scripts/Figures and Tables for manuscript.R
@@ -10,9 +10,36 @@ library("ggplot2")
 library("emmeans")
 library("ggpubr")
 library("plotrix") # for std.error function
+library("ggsignif")
 
 # Fig 1a: Bagged branch biomass ####
+biomass_summary <- read.csv("./Data/Models/model1.csv")
 
+# Arrange in order of native vs exotic
+biomass_order <- c("Beech","Musclewood","Shadbush","Striped Maple", "Sweet Birch", "Witch-hazel", "Autumn Olive", "Barberry", "Burning Bush", "Honeysuckle")
+biomass_summary <- arrange(transform(biomass_summary, tree=factor(tree,levels=biomass_order)), tree) 
+
+
+
+biomass_plot <- ggplot(data=biomass_summary, aes(x = tree, y = response, shape=exo)) +
+  theme_bw(base_size=16) +
+  geom_point(size=4.5) +
+  geom_errorbar(aes(ymin=response-(SE), ymax=response+(SE), width=0)) +
+  ylab("Average arthropod biomass on bagged branches (g)") +
+  xlab("Plant species") +
+  guides(shape=guide_legend(title="", title.position = "left")) +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(), axis.line = element_line(colour = "black")) +
+  theme(legend.position="bottom") +
+  geom_signif(comparisons = list(c("Shadbush", "Barberry")), 
+              y_position = 0.12,
+              tip_length = 0.21,
+              annotation = c("***")) +
+  geom_signif(y_position = c(0.11), 
+              xmin = c(0.9, 6.9), 
+              xmax = c(6.1, 10),
+    annotation = c(" ", " "), tip_length = 0.01)
+biomass_plot 
 
 
 
--- R Scripts/Models for manuscript.R
+++ R Scripts/Models for manuscript.R
@@ -17,10 +17,30 @@ ht_dat <- read.csv(file="./Data/Output/manuscript_dat.csv")
 # These are models that go into the main figures or are connected to the main text of the manuscript
 
 # Model 1: Bagged branch biomass #####
+model_1 <- lmer(log(wet_mass_g) ~ tree +  (1 | branch_code), data = subset(ht_dat, treatment="bag"))
+
+biomass_cld <- cld(emmeans(model_1, ~ tree, type="response"), adjust="scheffe", type="response")
+
+# pooled contrast of natives vs. non-natives using an emmeans reference grid
 
 
 
+# figure generation
+# write a summary table of totals and means for insects by host plant
+# mean, total, SEM
 
+biomass_summary <- ht_dat %>% 
+  filter(treatment == 'bag') %>%
+  group_by(tree,exo) %>% 
+  summarise(biomass_mean = mean(wet_mass_g), sem = std.error(wet_mass_g, na.rm=TRUE)) 
+
+# merge biomass cld with biomass_summary
+biomass_summary <- biomass_summary %>%
+  left_join(y=biomass_cld, by = c("tree"))%>%
+  as.data.frame()
+
+# write the arranged model outputs (both posthoc tests and mean/SE)
+write.csv(biomass_summary, "./Data/Models/model1.csv")
 
 
 
@@ -42,6 +62,32 @@ tree.lsm <- emmeans(model_2, ~ treatment | tree, type = "response") %>%
 
 # Write the lsm object to a .csv for ggplot to use
 write.csv(tree.lsm, "./Data/Models/model2.csv")
+
+
+# Model 3: Araneae #####
+
+# Model 4: Hemiptera ####
+
+# Model 5: Lepidoptera ####
+
+# Model 6: Orthoptera ####
+
+
+
+
+# Model 7: Herbivore N ####
+
+
+# Model 8: Herbivore CN ####
+
+
+# Model 9: Spider N ####
+
+# Model 10: Spider CN ####
+
+
+
+
 
 
 # Supplemental Analyses #####
